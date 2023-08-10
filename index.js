3be87594d11f0e9e7d891e9487e67cc295384c4e
admin <toan.nq194386@gmail.com>
2023-08-10 17:28:53 +0700

    u1

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ const client = new net.Socket();
 
 const io = require("socket.io-client");
 
-const clientSocket = io("127.0.0.1:8080"); // Replace with your server's URL
+const clientSocket = io("http://localhost:8080"); // Replace with your server's URL
 
 clientSocket.on("connect", () => {
   console.log("Connected to server");
@@ -47,7 +47,7 @@ clientSocket.on("connect", () => {
 clientSocket.on("data", (data) => {
   console.log(`Received from server: ${data.toString()}`);
   // Close the connection
-  client.end();
+  //   client.end();
 });
 
 // client.connect(8080, "127.0.0.1", () => {
